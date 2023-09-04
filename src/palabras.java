f68c7f39c9c5201b85eb229027c3a272f04c1e6b
Angel Stiven Velez Castaño <angelvelez1415@gmail.com>
2023-09-03 21:27:58 -0500

    Terminado

--- src/palabras.java
+++ src/palabras.java
@@ -1,5 +1,3 @@
-/*Realice un algoritmo que use un método sin parámetros y sin retorno que imprime en pantalla tu nombre completo*/
-
 /*7. Realiza un algoritmo donde se simule un diccionario simple. Crea un HashMap o HashTable donde las claves sean palabras en inglés y los valores sean las traducciones al español. Permite al usuario ingresar una palabra en inglés y muestra su traducción en español si está en el diccionario.*/
 import java.util.Hashtable;
 
@@ -10,14 +8,27 @@ public class palabras {
 	public static void main(String[] args) {
 		palabras diccionario=new palabras();
 		diccionario.palabrasEnDiccionario();
+		
+		String palabraEnIngles=JOptionPane.showInputDialog("Ingrese una palbra en ingles");
+		String traducida=diccionario.traduccion(palabraEnIngles);
+		
+		if (traducida != null) {
+			JOptionPane.showMessageDialog(null, "Palabra encontrada: "+palabraEnIngles+" y su traduccion es: "+traducida);
+		} else {
+			JOptionPane.showMessageDialog(null, "Palabra no encontrada");
+		}
+		
+		
 	}
+	private Hashtable<String, String> diccionario = new Hashtable<String, String>();
+	
 	public void palabrasEnDiccionario() {
-		Hashtable<String, String> palabra = new Hashtable<String, String>();
-		
-		palabra.put("Hello", "Hola");
-		palabra.put("Sorry", "Perdon");
-		palabra.put("Love", "Amor");
-		
-		JOptionPane.showMessageDialog(null, palabra.get("Hello"));
+	    diccionario.put("Hello", "Hola");
+	    diccionario.put("Sorry", "Perdon");
+	    diccionario.put("Love", "Amor");
+	}
+	
+	public String traduccion(String palabraEnIngles) {
+		return diccionario.get(palabraEnIngles);
 	}
 }
